2ed75ff94259e6727bbeeea50eb863e86744d7dc
Brock Lumbard <jlumbard@uwo.ca>
2020-10-29 14:17:57 -0600

    added gitignore, hid the config files we don't want available

--- FlaskServer/DocumentDB-Quickstart-Python/config.py
+++
@@ -1,8 +0,0 @@
-import os
-
-settings = {
-    'host': os.environ.get('ACCOUNT_HOST', 'https://jlumbard.documents.azure.com:443/'),
-    'master_key': os.environ.get('ACCOUNT_KEY', 'BlbQb65UIOausyjKvPH9o14ahuulkwYbDZ0Os9pmWOdWrREPJvmBR3O5ixx1uAZkEoAajj3tpAOTdjeuz8DXDw=='),
-    'database_id': os.environ.get('COSMOS_DATABASE', 'ToDoList'),
-    'container_id': os.environ.get('COSMOS_CONTAINER', 'Items'),
-}
--- FlaskServer/main.py
+++ FlaskServer/main.py
@@ -1,6 +1,7 @@
 from flask import Flask, flash, request, redirect, url_for, render_template, send_from_directory, session, jsonify, make_response
 import os
 import sys
+import UtilityFunctions.DBUtilities as DBUtilities
 
 #TO RUN:
 #export FLASK_APP=main.py
@@ -28,7 +29,7 @@ def homeTestPage():
 #Expected endpoints 
 
 
-@app.route('/getNextArticle')
+@app.route('/getNextArticle',methods=['GET'])
 def getNextArticle():
     #probably passes the userID or guid on their session. 
     #article they're currently on and their engagement with it should already be pushed, seperate endpoint.
@@ -38,11 +39,23 @@ def getNextArticle():
     return "NOT IMPLEMENTED"
 
 
-@app.route('/pushUserInteractionData')
+@app.route('/pushUserInteractionData',methods=['POST'])
 def pushUserInteraction():
+    checkUserCookie = request.form.get('sessionToken',None)
+
+    #Check User Token
+    DBUtilities.initiateDatabase()
+
+
+
+
     #passes info in a POST request about what the user did. No return type
     # how long did they spend on the article. What is the content of the article?
     # did they click on anything? 
 
     # THis data is all used to then push to the database so we know more about user tendencies. 
     return "NOT IMPLEMENTED"
+
+@app.route('/signUp',methods=['POST'])
+def signUp():
+    print("sign up ")
