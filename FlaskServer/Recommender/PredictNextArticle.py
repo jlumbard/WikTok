b27a83a2411fc89491a9b1294705c5a632880528
Ali Malik <amali28@uwo.ca>
2021-03-23 17:42:28 -0400

    Added comments

--- FlaskServer/Recommender/PredictNextArticle.py
+++ FlaskServer/Recommender/PredictNextArticle.py
@@ -44,19 +44,14 @@ def getPopularityBasedRecs():
     pageMetrics_data['pageViewsTrend_zscore'] = stats.zscore(pageViewsTrend)
     pageMetrics_data['Adjusted_Page_Views_zscore'] = pageMetrics_data['pageViews_zscore'] * 0.75
     pageMetrics_data['sumOfScores'] = pageMetrics_data['Adjusted_Page_Views_zscore'] + pageMetrics_data['pageViewsTrend_zscore']
-    # print(np.max(pageMetrics_data['Adjusted_Page_Views_zscore']))
     
     pageMetrics_data = pageMetrics_data.sort_values(by='sumOfScores', ascending=False)
-    # print(pageMetrics_data)
-    # Returns the top 15 articles
+
+    # Returns the top 3 articles
     similarArticles = pageMetrics_data.iloc[:3]['title'].values
 
     # Removes articles that the user has already read
     modifiedSimilarArticles =  removeArticlesRead(similarArticles)
-
-    # index = random.randint(0,len(modifiedSimilarArticles)-1)
-    # print("THE INDEX IS: " + str(index))
-    # print('THE URL IS: ' +modifiedSimilarArticles[index])
    
     return modifiedSimilarArticles
 
@@ -194,24 +189,16 @@ def collaborativeFiltering():
     "liked":{"0":0, "False": 0, "True": 2}}
     userRatingsDataDf = userRatingsDataDf.replace(numeric_categories)
     userRatingsDataDf['rating'] = userRatingsDataDf.timeSpent.values + userRatingsDataDf.liked.values
-    # print("RATINGS: ", userRatingsDataDf['rating'].values)
     ratingsTable = userRatingsDataDf.pivot_table(index=['article'], columns=['user'], values='rating').fillna(0)
-    # print("RATINGS TABLE: ", ratingsTable.values)
-    # print("Matrix before factorization: ")
-    # print(ratingsTable)
     mf = matrixFactorization(ratingsTable.values, K=3, alpha=0.1, beta=0.0001, iterations=200)
     mf.train()
-    # print("Matrix after factorization: ")
-    # print(mf.full_matrix())
     ratingsTable = ratingsTable.reset_index()
     columns = ratingsTable.columns.values.tolist()
     columns.pop(0) 
-    # print(columns)
     df = pd.DataFrame(mf.full_matrix(), columns = columns )
     df['article'] = ratingsTable['article'].values
     df.set_index('article', inplace = True)
     recommendedArticles = getCFArticles(df).tolist()
-    
 
     return recommendedArticles
 
@@ -224,23 +211,13 @@ def getCFArticles(df):
     filtered_df = filtered_df.sort_values(by=userID, ascending=False)
     recommendedArticles = filtered_df.iloc[:10]
     recommendedArticles = recommendedArticles.index.values
-
     return recommendedArticles
 
+# This class was adapted from: https://www.kaggle.com/rajaranjith1999/matrix-factoriztion
 class matrixFactorization():
 
     def __init__(self, R, K, alpha, beta, iterations):
-        """
-        Perform matrix factorization to predict empty
-        entries in a matrix.
-
-        Arguments
-        - R (ndarray)   : user-item rating matrix
-        - K (int)       : number of latent dimensions
-        - alpha (float) : learning rate
-        - beta (float)  : regularization parameter
-        """
-
+ 
         self.R = R
         self.num_users, self.num_items = R.shape
         self.K = K
@@ -249,16 +226,11 @@ class matrixFactorization():
         self.iterations = iterations
 
     def train(self):
-        # Initialize user and item latent feature matrice
         self.P = np.random.normal(scale=1./self.K, size=(self.num_users, self.K))
         self.Q = np.random.normal(scale=1./self.K, size=(self.num_items, self.K))
-
-        # Initialize the biases
         self.b_u = np.zeros(self.num_users)
         self.b_i = np.zeros(self.num_items)
         self.b = np.mean(self.R[np.where(self.R != 0)])
-
-        # Create a list of training samples
         self.samples = [
             (i, j, self.R[i, j])
             for i in range(self.num_users)
@@ -266,22 +238,16 @@ class matrixFactorization():
             if self.R[i, j] > 0
         ]
 
-        # Perform stochastic gradient descent for number of iterations
         training_process = []
         for i in range(self.iterations):
             np.random.shuffle(self.samples)
             self.sgd()
             mse = self.mse()
             training_process.append((i, mse))
-            # if (i+1) % 10 == 0:
-                # print("Iteration: %d ; Mean Squared Error = %.4f" % (i+1, mse))
 
         return training_process
 
     def mse(self):
-        """
-        A function to compute the total mean square error
-        """
         xs, ys = self.R.nonzero()
         predicted = self.full_matrix()
         error = 0
@@ -290,34 +256,26 @@ class matrixFactorization():
         return np.sqrt(error)
 
     def sgd(self):
-        """
-        Perform stochastic gradient descent
-        """
         for i, j, r in self.samples:
-            # Computer prediction and error
+          
             prediction = self.get_rating(i, j)
             e = (r - prediction)
 
-            # Update biases
+    
             self.b_u[i] += self.alpha * (e - self.beta * self.b_u[i])
             self.b_i[j] += self.alpha * (e - self.beta * self.b_i[j])
 
-            # Update user and item latent feature matrices
             self.P[i, :] += self.alpha * (e * self.Q[j, :] - self.beta * self.P[i,:])
             self.Q[j, :] += self.alpha * (e * self.P[i, :] - self.beta * self.Q[j,:])
 
     def get_rating(self, i, j):
-        """
-        Get the predicted rating of user i and item j
-        """
         prediction = self.b + self.b_u[i] + self.b_i[j] + self.P[i, :].dot(self.Q[j, :].T)
         return prediction
 
     def full_matrix(self):
-        """
-        Computer the full matrix using the resultant biases, P and Q
-        """
         return self.b + self.b_u[:,np.newaxis] + self.b_i[np.newaxis:,] + self.P.dot(self.Q.T)
+
+
 
 # return list of articles
 def returnRecsForSideTab(currentURL, userID):
